40ab5e845b008d905cebdae4746837062d1c66c9
Nicolas Gasull <nicolas@gasull.net>
2025-03-03 12:11:50 +0100

    Flakify

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ deno ];
+        };
+      });
+}
